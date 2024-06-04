9e1e2bff2b657ade5b64fe81199abb0ae8b7e852
Rafael Gomes <rafael.gcaldas01@gmail.com>
2024-06-03 23:11:22 -0300

    feat: Get user membership

--- apps/api/@types/fastify.d.ts
+++ apps/api/@types/fastify.d.ts
@@ -1,7 +1,12 @@
 import 'fastify'
 
+import { Member, Organization } from '@prisma/client'
+
 declare module 'fastify' {
   export interface FastifyRequest {
     getCurrentUserId(): Promise<string>
+    getUserMemberShip(
+      slug: string,
+    ): Promise<{ organization: Organization; memberShip: Member }>
   }
 }
