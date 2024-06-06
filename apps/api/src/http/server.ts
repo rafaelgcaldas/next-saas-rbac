a27bf53f8ba89df7bd64f336c3a6087c257fe484
Rafael Gomes <rafael.gcaldas01@gmail.com>
2024-06-05 23:38:07 -0300

    feat: Get all organization members

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -18,6 +18,7 @@ import { createAccsount } from './routes/auth/create-account'
 import { getProfile } from './routes/auth/get-profile'
 import { requestPasswordRecover } from './routes/auth/request-password-recover'
 import { resetPassword } from './routes/auth/reset-password'
+import { getMembers } from './routes/members/get-members'
 import { createOrganization } from './routes/orgs/create-organization'
 import { getMembership } from './routes/orgs/get-membership'
 import { getOrganization } from './routes/orgs/get-organization'
@@ -87,6 +88,8 @@ app.register(deleteProject)
 app.register(getProject)
 app.register(getProjects)
 app.register(updateProject)
+
+app.register(getMembers)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log('Http server running!')
