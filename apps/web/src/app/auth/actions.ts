ad67ad22a9e28bc8748e2f68b8c87f5c82fafea2
Rafael Gomes <rafael.gcaldas01@gmail.com>
2024-06-12 23:18:57 -0300

    feat: Login with Github

---
+++ apps/web/src/app/auth/actions.ts
@@ -0,0 +1,16 @@
+'use serve'
+
+import { redirect } from 'next/navigation'
+
+export async function signInWithGithub() {
+  const githubSignInURL = new URL('login/oauth/authorize', 'https://github.com')
+
+  githubSignInURL.searchParams.set('client_id', 'Ov23lirI860h9x2GbSEd')
+  githubSignInURL.searchParams.set(
+    'redirect_uri',
+    'http://localhost:3000/api/auth/callback',
+  )
+  githubSignInURL.searchParams.set('scope', 'user')
+
+  redirect(githubSignInURL.toString())
+}
