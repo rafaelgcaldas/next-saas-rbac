64fdbdb7a8b5f064f8ed010ffef3563116ebc5f1
Rafael Gomes <rafael.gcaldas01@gmail.com>
2024-06-21 23:51:06 -0300

    feat: List organizations

--- apps/web/src/components/organization-swicher.tsx
+++ apps/web/src/components/organization-swicher.tsx
@@ -1,6 +1,8 @@
 import { ChevronsUpDown, PlusCircle } from 'lucide-react'
 import Link from 'next/link'
 
+import { getOrganizations } from '@/http/get-organizations'
+
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
 import {
   DropdownMenu,
@@ -12,7 +14,8 @@ import {
   DropdownMenuTrigger,
 } from './ui/dropdown-menu'
 
-export function OrganizatioSwicher() {
+export async function OrganizatioSwicher() {
+  const { organizations } = await getOrganizations()
   return (
     <DropdownMenu>
       <DropdownMenuTrigger className="w=[168px] flex items-center gap-2 rounded p-1 text-sm font-medium outline-none focus-visible:ring-2 focus-visible:ring-primary">
@@ -27,13 +30,21 @@ export function OrganizatioSwicher() {
       >
         <DropdownMenuGroup>
           <DropdownMenuLabel>Organizations</DropdownMenuLabel>
-          <DropdownMenuItem>
-            <Avatar className="mr-2 size-4">
-              <AvatarImage src="https://github.com/rocketseat.png" />
-              <AvatarFallback />
-            </Avatar>
-            <span className="line-clamp-1">Rocketseat</span>
-          </DropdownMenuItem>
+          {organizations.map((organization) => {
+            return (
+              <DropdownMenuItem key={organization.id} asChild>
+                <Link href={`/org/${organization.slug}`}>
+                  <Avatar className="mr-2 size-4">
+                    {organization.avatarUrl && (
+                      <AvatarImage src={organization.avatarUrl} />
+                    )}
+                    <AvatarFallback />
+                  </Avatar>
+                  <span className="line-clamp-1">{organization.name}</span>
+                </Link>
+              </DropdownMenuItem>
+            )
+          })}
         </DropdownMenuGroup>
         <DropdownMenuSeparator />
         <DropdownMenuItem asChild>
