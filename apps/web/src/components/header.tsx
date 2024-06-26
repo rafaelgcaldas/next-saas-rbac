55a28e71297aceff57cb92887a07c93459f119ee
Rafael Gomes <rafael.gcaldas01@gmail.com>
2024-06-25 23:20:23 -0300

    feat: Dark/light theme

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -6,6 +6,8 @@ import { ability } from '@/auth/auth'
 
 import { OrganizatioSwicher } from './organization-swicher'
 import { ProfileButton } from './profile-button'
+import { ThemeSwitcher } from './theme/theme-switcher'
+import { Separator } from './ui/separator'
 
 export async function Header() {
   const permissions = await ability()
@@ -25,6 +27,8 @@ export async function Header() {
       </div>
 
       <div className="flex items-center gap-4">
+        <ThemeSwitcher />
+        <Separator orientation="vertical" className="h-5" />
         <ProfileButton />
       </div>
     </div>
