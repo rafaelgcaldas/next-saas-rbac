5d131f808df5f2168da09cb4fdd9a81afebd66de
Rafael Gomes <rafael.gcaldas01@gmail.com>
2024-07-19 23:15:34 -0300

    feat: Project switcher component

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -6,6 +6,7 @@ import { ability } from '@/auth/auth'
 
 import { OrganizatioSwicher } from './organization-swicher'
 import { ProfileButton } from './profile-button'
+import { ProjectSwitcher } from './project-switcher'
 import { ThemeSwitcher } from './theme/theme-switcher'
 import { Separator } from './ui/separator'
 
@@ -23,7 +24,12 @@ export async function Header() {
 
         <OrganizatioSwicher />
 
-        {permissions?.can('get', 'Project') && <p>Projetos</p>}
+        {permissions?.can('get', 'Project') && (
+          <>
+            <Slash className="size-3 -rotate-[24deg] text-border" />
+            <ProjectSwitcher />
+          </>
+        )}
       </div>
 
       <div className="flex items-center gap-4">
