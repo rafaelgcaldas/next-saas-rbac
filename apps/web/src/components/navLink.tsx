7edfa61c4a1f25b7c06a1ac91f03ea92ed1a1e60
Rafael Gomes <rafael.gcaldas01@gmail.com>
2024-07-24 16:10:33 -0300

    feat: Create NavLink component

---
+++ apps/web/src/components/navLink.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import type { ComponentProps } from 'react'
+
+interface NavLinkProps extends ComponentProps<typeof Link> {}
+
+export function NavLink(props: NavLinkProps) {
+  const pathName = usePathname()
+  const isCurrent = props.href.toString() === pathName
+
+  return <Link data-current={isCurrent} {...props} />
+}
